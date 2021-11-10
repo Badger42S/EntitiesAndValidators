1d207bc411962392c851a437ba2a9ad34061916d
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 22:39:56 +0300

    fix: clean code CustomerValidator

--- src/Validators/CustomerValidator.cs
+++ src/Validators/CustomerValidator.cs
@@ -12,9 +12,7 @@ namespace Validators
             RuleFor(customer => customer.LastName).MaximumLength(50).WithMessage("Last name must be shorter than 50 characters");
             RuleFor(customer => customer.FirstName).MaximumLength(50).WithMessage("First name must be shorter than 50 characters");
             RuleFor(customer => customer.Addresses).NotEmpty().WithMessage("Required at least 1 address");
-
             RuleForEach(customer => customer.Addresses).SetValidator(new AddressValidator());
-
             RuleFor(customer => customer.Email).Matches(@"^[a-z0-9+_.-]+@[a-z0-9.-]+\.[a-z]{1,5}$").
                 When(customer => customer.Email.Length != 0).WithMessage("Incorrect email address entered");
             RuleFor(customer => customer.PhoneNumber).Matches(@"^\+?[1-9]\d{1,14}$").
