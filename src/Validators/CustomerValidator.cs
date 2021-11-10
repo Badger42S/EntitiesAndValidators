b18d67ebf6db74ad87f29a3e05516d0a5a0665d3
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 16:51:00 +0300

    fix: the phone check has been changed

--- src/Validators/CustomerValidator.cs
+++ src/Validators/CustomerValidator.cs
@@ -38,12 +38,13 @@ namespace Validators
                 validateCustomerErrors.AddRange(AddressValidator.Validate(address));
             }
             //valid email
-            if (!Regex.IsMatch(customer.Email, @"^[a-z0-9+_.-]+@[a-z0-9.-]+\.[a-z]{1,3}$", RegexOptions.IgnoreCase))
+            if (customer.Email.Length !=0 && 
+                !Regex.IsMatch(customer.Email, @"^[a-z0-9+_.-]+@[a-z0-9.-]+\.[a-z]{1,5}$", RegexOptions.IgnoreCase))
             {
                 validateCustomerErrors.Add("Incorrect email address entered");
             }
             //valid phone number
-            if (!Regex.IsMatch(customer.PhoneNumber, @"^\+?[1-9]\d{1,14}$"))
+            if (customer.PhoneNumber.Length != 0 && !Regex.IsMatch(customer.PhoneNumber, @"^\+?[1-9]\d{1,14}$"))
             {
                 validateCustomerErrors.Add("Incorrect phone number entered");
             }
