19bfded3b8c3ae463b2fe8342db94f791cf21805
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 13:32:19 +0300

    feat: adresses class

--- src/AddressRecord/Address .cs
+++ src/AddressRecord/Address .cs
@@ -4,15 +4,28 @@ namespace AddressRecord
 {
     public class Address
     {
-        public enum AddressType
+        public enum AddressTypeEnum
         {
             Shipping,
             Billing
         }
-        public Address(string addressLine, AddressType addressType, string city, 
+        public string AddressLine { get; set; }
+        public string AddressLine2 { get; set; }
+        public AddressTypeEnum AddressType { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public Address(string addressLine, AddressTypeEnum addressType, string city, 
             string postalCode, string state, string country, string addressLine2 = "")
         {
-
+            AddressLine = addressLine;
+            AddressLine2 = addressLine2;
+            AddressType = addressType;
+            City = city;
+            PostalCode = postalCode;
+            State = state;
+            Country = country;
         }
     }
 }
