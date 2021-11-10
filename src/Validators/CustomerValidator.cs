8711a6d0a93833106fd2199831f1fc19982d2a0f
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 14:06:56 +0300

    feat: address validator

--- src/Validators/CustomerValidator.cs
+++ src/Validators/CustomerValidator.cs
@@ -11,81 +11,49 @@ namespace Validators
     {
         public static List<string> Validate(Customer customer)
         {
-            List<string> errors = new() { };
+            List<string> validateCustomerErrors = new() { };
             //Customer check
+            //LastName lenght
             if (customer.LastName.Length > 50)
             {
-                errors.Add("Last name must be shorter than 50 characters");
+                validateCustomerErrors.Add("Last name must be shorter than 50 characters");
             }
+            //LastName required
             if (customer.LastName.Length == 0)
             {
-                errors.Add("Last name required");
+                validateCustomerErrors.Add("Last name required");
             }
+            //FirstName lenght
             if (customer.FirstName.Length > 50)
             {
-                errors.Add("First name must be shorter than 50 characters");
+                validateCustomerErrors.Add("First name must be shorter than 50 characters");
             }
+            //Adresses count
             if (customer.Addresses.Count == 0)
             {
-                errors.Add("Required at least 1 address");
+                validateCustomerErrors.Add("Required at least 1 address");
             }
-            if(!Regex.IsMatch(customer.Email, @"\A[A-Z0-9+_.-]+@[A-Z0-9.-]+\Z"))
+            //Adress list check
+
+
+
+            //valid email
+            if (!Regex.IsMatch(customer.Email, @"\A[A-Z0-9+_.-]+@[A-Z0-9.-]+\Z"))
             {
-                errors.Add("Incorrect email address entered");
+                validateCustomerErrors.Add("Incorrect email address entered");
             }
+            //valid phone number
             if (!Regex.IsMatch(customer.PhoneNumber, @"^\+?[1-9]\d{1,14}$"))
             {
-                errors.Add("Incorrect phone number entered");
+                validateCustomerErrors.Add("Incorrect phone number entered");
             }
+            //Notes count
             if (customer.Notes.Count == 0)
             {
-                errors.Add("Required at least 1 note");
-            }
-            //Adress check
-            if (customer.Addresses.Any(adress => adress.AddressLine.Length == 0))
-            {
-                errors.Add("Address line required");
-            }
-            if (customer.Addresses.Any(adress => adress.AddressLine.Length > 100))
-            {
-                errors.Add("Address line must be shorter than 100 characters");
-            }
-            if (customer.Addresses.Any(adress => adress.AddressLine2.Length > 100))
-            {
-                errors.Add("Address line2 must be shorter than 100 characters");
-            }
-            if (customer.Addresses.Any(adress => adress.AddressType != Address.AddressTypeEnum.Billing ||
-                adress.AddressType != Address.AddressTypeEnum.Shipping))
-            {
-                errors.Add("Incorrect address type entered");
-            }
-            if (customer.Addresses.Any(adress => adress.City.Length == 0))
-            {
-                errors.Add("City required");
-            }
-            if (customer.Addresses.Any(adress => adress.City.Length > 50))
-            {
-                errors.Add("The name of the city must be shorter than 50 characters");
-            }
-            if (customer.Addresses.Any(adress => adress.PostalCode.Length > 6))
-            {
-                errors.Add("Postal code must be shorter than 6 characters");
-            }
-            if (customer.Addresses.Any(adress => adress.State.Length == 0))
-            {
-                errors.Add("State required");
-            }
-            if (customer.Addresses.Any(adress => adress.State.Length > 20))
-            {
-                errors.Add("The name of the state should be shorter than 20 characters");
-            }
-            if (customer.Addresses.Any(adress => adress.Country != "Canada" ||
-                adress.Country != "United States"))
-            {
-                errors.Add("Incorrect country entered. It should be Canada or United States");
+                validateCustomerErrors.Add("Required at least 1 note");
             }
 
-            return errors;
+            return validateCustomerErrors;
         }
     }
 }
